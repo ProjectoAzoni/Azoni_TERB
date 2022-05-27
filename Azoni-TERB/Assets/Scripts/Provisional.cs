ca3038259a8f2c1312254b285d34fe9b32b30d89
javierpenaf <ejpf2004@gmail.com>
2022-05-26 22:08:53 -0500

    ,

--- Azoni-TERB/Assets/Scripts/Provisional.cs
+++ Azoni-TERB/Assets/Scripts/Provisional.cs
@@ -6,6 +6,7 @@ public class Provisional : MonoBehaviour
 {
     public GameObject boton;
     public GameObject level2;
+    public GameObject smog;
     public int var=0;
 
     private void Update()
@@ -15,11 +16,13 @@ public class Provisional : MonoBehaviour
         {
             boton.SetActive(false);
             level2.SetActive(false);
+            smog.SetActive(true);
         }
         else
         {
             boton.SetActive(true);
             level2.SetActive(true);
+            smog.SetActive(false);
         }
 
     }
